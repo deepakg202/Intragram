f0d657e5fadb13580d665945b24007d59eec332a
Deepak Gupta <deepakgupta191199@gmail.com>
2020-05-25 18:41:33 +0530

    .htaccess added with temp error page

---
+++ error.php
@@ -0,0 +1,20 @@
+<link href="./css/bootstrap.min.css" rel="stylesheet">
+<style>
+body {
+    background: #dedede;
+}
+.page-wrap {
+    min-height: 100vh;
+}
+</style>
+<div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <span class="display-1 d-block">LOL!</span>
+                <div class="mb-4 lead">Unfortunately, An Error Occured.</div>
+                <a href="<?php echo dirname(__FILE__);?>" class="btn btn-link">Back to Home</a>
+            </div>
+        </div>
+    </div>
+</div>
